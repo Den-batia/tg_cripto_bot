d6107277a0a0dfd1646df2c920baa02801a3406f
Anton <anton.brazovski@gmail.com>
2020-09-01 18:22:01 +0300

    upd

---
+++ trading_api/api/tests/test_get_symbol_list.py
@@ -0,0 +1,28 @@
+from rest_framework.reverse import reverse
+from rest_framework.status import HTTP_200_OK
+
+from api.api import SymbolsViewSet
+from api.models import Symbol
+from api.tests.abstract_test import AbstractAPITestCase
+
+
+class SymbolReadonlyTest(AbstractAPITestCase):
+    def _get_uri(self, **kwargs):
+        return reverse('symbol-detail', **kwargs)
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.view_list = SymbolsViewSet.as_view({'get': 'list'})
+        self.view_detail = SymbolsViewSet.as_view({'get': 'detail'})
+        self.uri = reverse('symbol-list')
+
+    def test_list(self):
+        count = Symbol.objects.count()
+        response = self._make_get_request(view=self.view_list, uri=self.view_list)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(len(response.data), count)
+
+    def test_detail(self):
+        response = self._make_get_request(view=self.view_detail, uri=self._get_uri(id=1))
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'eth')
--- trading_api/manage.py
+++ trading_api/manage.py
@@ -6,6 +6,17 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trading_api.settings')
+
+    from django.conf import settings
+
+    running_tests = 'test' in sys.argv
+
+    if running_tests:
+        from coverage import Coverage
+        cov = Coverage()
+        cov.erase()
+        cov.start()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,7 +25,36 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    if running_tests:
+        import logging
+        logging.disable(logging.CRITICAL)
+        settings.DEBUG = False
+        settings.TEMPLATE_DEBUG = False
+        settings.PASSWORD_HASHERS = [
+            'django.contrib.auth.hashers.MD5PasswordHasher',
+        ]
+        settings.DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'test_database',
+            }
+        }
+        settings.MIDDLEWARE_CLASSES = [
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+        ]
+
     execute_from_command_line(sys.argv)
+
+    if running_tests:
+        cov.stop()
+        cov.save()
+        covered = cov.report()
+        if covered < 90:
+            sys.exit(1)
 
 
 if __name__ == '__main__':
