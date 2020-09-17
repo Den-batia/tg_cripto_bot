4e386b76585580a54b30d167618ae02a91020790
Anton <anton.brazovski@gmail.com>
2020-09-17 19:44:34 +0300

    upd

--- trading_api/routines/tasks/crypto/eth/confirm_withdraw.py
+++ trading_api/routines/tasks/crypto/eth/confirm_withdraw.py
@@ -10,8 +10,11 @@ logger = logging.getLogger('confirm_eth_withdraw')
 
 
 def confirm():
-    symbol = Symbol.objects.get(name='eth')
-    withdrawing_tx: Withdraw = symbol.withdraws.filter(tx_hash__isnull=False, confirmed_at__isnull=True).first()
+    withdrawing_tx: Withdraw = Withdraw.objects.filter(
+        tx_hash__isnull=False,
+        confirmed_at__isnull=True,
+        symbol__name__in=('eth', 'usdt')
+    ).first()
     if withdrawing_tx is not None and ETH.is_transaction_delivered(withdrawing_tx.tx_hash):
         withdrawing_tx.confirmed_at = datetime.now(timezone.utc)
         withdrawing_tx.save()
