55b5f9cc577413ca975d7981638f75e5aceaa458
Anton <anton.brazovski@gmail.com>
2020-09-26 16:06:29 +0300

    upd

--- bot/urls/edit_order.py
+++ bot/urls/edit_order.py
@@ -39,7 +39,7 @@ async def cancel_send_message(message: types.Message, state: FSMContext):
 @dp.message_handler(lambda msg: re.match(r'^-?[0-9]{1,2}%$', msg.text), state=EDIT_ORDER_RATE)
 @dp.message_handler(lambda msg: re.match(r'^-?[0-9.]{1,3}%$', msg.text), state=EDIT_ORDER_RATE)
 async def edit_rate_percents(message: types.Message, state):
-    coefficient = str(round((int(message.text[:-1]) + 100) / 100, 2))
+    coefficient = str(round((float(message.text[:-1]) + 100) / 100, 3))
     data = await state.get_data()
     text, k = await dh.update_order(message.from_user.id, data['order_id'], {'coefficient': coefficient})
     await send_message(text=text, chat_id=message.from_user.id, reply_markup=k)
