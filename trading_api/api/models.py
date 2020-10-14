d19925123f8dccfe2b98351f38de0087097ab213
Anton <anton.brazovski@gmail.com>
2020-10-14 11:08:27 +0300

    upd

--- trading_api/api/models.py
+++ trading_api/api/models.py
@@ -58,6 +58,9 @@ class Symbol(models.Model):
     deals_commission = models.DecimalField(max_digits=5, decimal_places=5, default=0.001)
     is_active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name.upper()
+
 
 class Account(models.Model):
     id = models.AutoField(primary_key=True)
@@ -76,6 +79,9 @@ class Account(models.Model):
 class Broker(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
