b5ddab4ecf52fdd046f3938ad3af6b696f7a6852
Anton <anton.brazovski@gmail.com>
2020-10-18 22:36:42 +0300

    upd

--- trading_api/routines/tasks/crypto/prizm/process_deposit.py
+++ trading_api/routines/tasks/crypto/prizm/process_deposit.py
@@ -13,7 +13,7 @@ def process_deposit_prizm():
     symbol = Symbol.objects.get(name='prizm')
     for dep in symbol.deposits.filter(confirmed_at__isnull=True):
         tx = PRIZM.get_tx(dep.tx_hash)
-        print(tx)
+        print(tx, dep.tx_hash)
         if tx['confirmations'] > 0:
             with atomic():
                 account = dep.user.accounts.filter(symbol=symbol).first()
