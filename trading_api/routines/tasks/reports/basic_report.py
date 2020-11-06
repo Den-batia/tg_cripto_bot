807001bc5e882d84efee9bb174bf8698e1f70c2a
Anton <anton.brazovski@gmail.com>
2020-11-06 18:35:44 +0300

    upd

--- trading_api/routines/tasks/reports/basic_report.py
+++ trading_api/routines/tasks/reports/basic_report.py
@@ -15,8 +15,8 @@ def send_report():
     users_stat = UsersStatView.get_users_stat()
     deals_stat = DealsStatView.get_deals_stat()
     text = "<b>Баланс:</b>\n"
-    for symbol, val in balance:
-        text += f"{symbol}:\nБлокчейн: {val['wallet']} {symbol}\nБаза данных: {val['db']} {symbol}\nБаланс: {val['balance']} {symbol}\n\n"
+    for item in balance:
+        text += f"{item['symbol']}:\nБлокчейн: {item['wallet']} {item['symbol']}\nБаза данных: {item['db']} {item['symbol']}\nБаланс: {item['balance']} {item['symbol']}\n\n"
 
     text += '\n<b>Пользователи:</b>\n'
     for key, value in users_stat.items():
